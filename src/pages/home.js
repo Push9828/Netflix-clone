851bef44285156c8f01b79b8e64a4a8d26227817
Pushkar <pushkar.thakur28@gmail.com>
2022-04-13 21:19:23 +0530

    add header

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,5 @@
+import { Feature, OptForm } from "../components";
+import { HeaderContainer } from "../containers/header";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FooterContainer } from "../containers/footer";
 import { FaqContainer } from "../containers/faq";
@@ -5,6 +7,21 @@ import { FaqContainer } from "../containers/faq";
 export default function Home() {
   return (
     <>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>Unlimited movies, TV shows and more.</Feature.Title>
+          <Feature.SubTitle>Watch anywhere. Cancel anytime.</Feature.SubTitle>
+          <OptForm>
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </OptForm.Text>
+            <OptForm.Break />
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Get Started</OptForm.Button>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqContainer />
       <FooterContainer />
